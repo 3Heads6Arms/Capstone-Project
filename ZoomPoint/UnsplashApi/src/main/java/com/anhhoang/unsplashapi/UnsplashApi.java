f1b7fadf65aa0fd40d458e5410dea9279d0e3aee
Anh.Hoang <h.anh.viet91@gmail.com>
2017-11-24 17:49:05 +0200

    Implemented Api calls

--- ZoomPoint/UnsplashApi/src/main/java/com/anhhoang/unsplashapi/UnsplashApi.java
+++ ZoomPoint/UnsplashApi/src/main/java/com/anhhoang/unsplashapi/UnsplashApi.java
@@ -1,6 +1,11 @@
 package com.anhhoang.unsplashapi;
 
+import com.anhhoang.unsplashmodel.Photo;
+import com.anhhoang.unsplashmodel.PhotoCollection;
+import com.anhhoang.unsplashmodel.UserProfile;
+
 import java.io.IOException;
+import java.util.List;
 
 import okhttp3.Authenticator;
 import okhttp3.Credentials;
@@ -8,6 +13,8 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.Route;
+import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -27,22 +34,82 @@ public class UnsplashApi {
         return INSTANCE;
     }
 
-    private Retrofit retrofit;
     private UnsplashApiService unsplashApiService;
-    private String token;
 
     private UnsplashApi(String token) {
-        this.token = token;
-
         OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(new AuthorizationInterceptor(this.token))
+                .addInterceptor(new AuthorizationInterceptor(token))
                 .build();
 
-        retrofit = new Retrofit.Builder()
+        Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(API_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build();
         unsplashApiService = retrofit.create(UnsplashApiService.class);
+    }
+
+    public Call<List<Photo>> getPhotos(int page, int perPage, String orderBy) {
+        return unsplashApiService.getPhotos(page, perPage, orderBy);
+    }
+
+    public Call<List<Photo>> getCuratedPhotos(int page, int perPage, Callback<List<Photo>> calback) {
+        return unsplashApiService.getCuratedPhotos(page, perPage);
+    }
+
+    public Call<Photo> getPhoto(String photoId, Callback<Photo> callback) {
+        return unsplashApiService.getPhoto(photoId);
+    }
+
+    public Call<Photo> getRandomPhoto(boolean featured) {
+        return unsplashApiService.getRandomPhoto(featured);
+    }
+
+    public Call<Photo> likePhoto(String photoId) {
+        return unsplashApiService.likePhoto(photoId);
+    }
+
+    public Call<Photo> unlikePhoto(String photoId) {
+        return unsplashApiService.unlikePhoto(photoId);
+    }
+
+    public Call<List<Photo>> searchPhotos(String searchTerms, int page, int perPage) {
+        return unsplashApiService.searchPhotos(searchTerms, page, perPage);
+    }
+
+    public Call<List<PhotoCollection>> getFeaturedCollections(int page, int perPage) {
+        return unsplashApiService.getFeaturedCollections(page, perPage);
+    }
+
+    public Call<List<Photo>> getCollectionPhotos(long collectionId, int page, int perPage) {
+        return unsplashApiService.getCollectionPhotos(collectionId, page, perPage);
+    }
+
+    public Call<PhotoCollection> createCollection(PhotoCollection collection) {
+        return unsplashApiService.createCollection(collection);
+    }
+
+    public Call<Photo> addPhotoToCollection(long collectionId, String photoId) {
+        return unsplashApiService.addPhotoToCollection(collectionId, photoId);
+    }
+
+    public Call<Photo> removePhotoFromCollection(long collectionId, String photoId) {
+        return unsplashApiService.removePhotoFromCollection(collectionId, photoId);
+    }
+
+    public Call<UserProfile> getUserProfile(String username) {
+        return unsplashApiService.getUserProfile(username);
+    }
+
+    public Call<List<PhotoCollection>> getUserCollections(String username) {
+        return unsplashApiService.getUserCollections(username);
+    }
+
+    public Call<List<Photo>> getUserLikedPhotos(String username, int page, int perPage) {
+        return unsplashApiService.getUserLikedPhotos(username, page, perPage);
+    }
+
+    public Call<List<Photo>> getUserPhotos(String username, int page, int perPage) {
+        return unsplashApiService.getUserPhotos(username, page, perPage);
     }
 }
--- ZoomPoint/UnsplashApi/src/main/java/com/anhhoang/unsplashapi/UnsplashAuthApi.java
+++ ZoomPoint/UnsplashApi/src/main/java/com/anhhoang/unsplashapi/UnsplashAuthApi.java
@@ -1,5 +1,9 @@
 package com.anhhoang.unsplashapi;
 
+import com.anhhoang.unsplashmodel.authmodel.TokenRequest;
+import com.anhhoang.unsplashmodel.authmodel.TokenResponse;
+
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -19,14 +23,17 @@ public class UnsplashAuthApi {
         return INSTANCE;
     }
 
-    private Retrofit retrofit;
     private UnsplashAuthApiService unsplashApiService;
 
     private UnsplashAuthApi() {
-        retrofit = new Retrofit.Builder()
+        Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(API_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         unsplashApiService = retrofit.create(UnsplashAuthApiService.class);
+    }
+
+    public Call<TokenResponse> getToken(TokenRequest request) {
+        return unsplashApiService.getToken(request);
     }
 }

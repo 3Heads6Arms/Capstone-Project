ebab311884037a93fd2ae869ffb64ff1ff2662a0
anh.hoang <h.anh.viet91@gmail.com>
2017-11-25 14:45:35 +0200

    added DB module

---
+++ ZoomPoint/Database/src/main/java/com/anhhoang/database/ZoomPointContract.java
@@ -0,0 +1,37 @@
+package com.anhhoang.database;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by anh.hoang on 25.11.17.
+ */
+
+public class ZoomPointContract {
+    public static final String CONTENT_AUTHORITY = "com.anhhoang.zoompoint";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_PHOTO = "photo";
+    public static final String PATH_COLLECTION = "collection";
+
+    public static final class PhotoEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PHOTO).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PHOTO;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PHOTO;
+
+        public static final String TABLE_NAME = "photos";
+    }
+
+    public static final class CollectionEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_COLLECTION).build();
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_COLLECTION;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_COLLECTION;
+
+        public static final String TABLE_NAME = "collections";
+    }
+}

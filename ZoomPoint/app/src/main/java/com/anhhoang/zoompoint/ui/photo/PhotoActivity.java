338482b888f426cb6708c494830a27c31778b800
anh.hoang <h.anh.viet91@gmail.com>
2017-12-28 18:20:03 +0200

    Finalized PhotoActivity logic to open PhotoFragment

--- ZoomPoint/app/src/main/java/com/anhhoang/zoompoint/ui/photo/PhotoActivity.java
+++ ZoomPoint/app/src/main/java/com/anhhoang/zoompoint/ui/photo/PhotoActivity.java
@@ -3,12 +3,10 @@ package com.anhhoang.zoompoint.ui.photo;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.view.View;
+import android.view.MenuItem;
 
 import com.anhhoang.zoompoint.R;
 
@@ -24,22 +22,29 @@ public class PhotoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_photo);
 
         Intent intent = getIntent();
-
         String photoId = intent.getStringExtra(PHOTO_ID_KEY);
-
         checkArgument(!TextUtils.isEmpty(photoId), "PhotoId parameter cannot be null");
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if (savedInstanceState == null) {
+            Fragment fragment = new PhotoFragment();
+            fragment.setArguments(PhotoFragment.getStartingBundle(photoId));
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_photo, fragment, TAG)
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public static Intent getStartingIntent(Context context, String photoId) {
--- ZoomPoint/app/src/main/java/com/anhhoang/zoompoint/ui/photo/PhotoFragment.java
+++ ZoomPoint/app/src/main/java/com/anhhoang/zoompoint/ui/photo/PhotoFragment.java
@@ -1,24 +1,64 @@
 package com.anhhoang.zoompoint.ui.photo;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.anhhoang.zoompoint.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PhotoFragment extends Fragment {
+    private static final String PHOTO_ID_KEY = "PhotoIdKey";
+    private String photoId;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     public PhotoFragment() {
+        setRetainInstance(true);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle bundle = getArguments();
+        checkNotNull(bundle);
+        photoId = bundle.getString(PHOTO_ID_KEY);
+        checkArgument(!TextUtils.isEmpty(photoId), "Photo Id must be passed as parameter");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_photo, container, false);
+        View view = inflater.inflate(R.layout.fragment_photo, container, false);
+        ButterKnife.bind(this, view);
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        return view;
+    }
+
+    public static Bundle getStartingBundle(String photoId) {
+        Bundle bundle = new Bundle();
+        bundle.putString(PHOTO_ID_KEY, photoId);
+
+        return bundle;
     }
 }
